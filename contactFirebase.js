1e4f0eb66ac082f3d360644ad753001f71bab463
Deepesh Agarwal <deepeshagarwal1116@gmail.com>
2024-01-21 04:53:51 +0530

    ramcf

---
+++ contactFirebase.js
@@ -0,0 +1,47 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmiFBPEC3ALqmNMIvJe87SvRGuXcOxQhY",
+    authDomain: "plan-it-contact.firebaseapp.com",
+    projectId: "plan-it-contact",
+    storageBucket: "plan-it-contact.appspot.com",
+    messagingSenderId: "111214089914",
+    appId: "1:111214089914:web:e7695f9d0bc604bb37d483",
+    databaseURL:"https://plan-it-contact-default-rtdb.firebaseio.com"
+  };
+
+  
+
+firebase.initializeApp(firebaseConfig);
+
+
+var messagesRef = firebase.database().ref('messages');
+
+document.getElementById("contact").addEventListener('submit',submitForm);
+
+
+async function submitForm(e){
+    e.preventDefault();
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var subject = getInputVal('subject');
+    var message = getInputVal('message');
+
+    await saveMessage(name, email, subject,message);
+    alert("Your Response has been recorded.\nThanks For Your Response 💓🙂");
+    // Clear form
+    document.getElementById('contact').reset();
+  }
+  
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, email, subject,message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email:email,
+      subject:subject,
+      message:message
+    });
+  }
